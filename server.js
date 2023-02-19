4898e70a1969b0560a716b75c102f73290b67119
Abdelrehman <abdelrehmanmamdouh@gmail.com>
2023-02-19 11:20:30 +0200

    Handeled Uncaught execptions and unhandeled rejections and automated postman authentication requsts

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXECPTION! Shutting down...');
+
+  // 0 for success and 1 for unhandled rejection here it's nessacry to shut down the server because the node process is not clean 
+   process.exit(1);
+});
+
 const app = require('./app');
 
 // Conecting to DB
@@ -21,4 +30,14 @@ mongoose
 const port = process.env.PORT || 3000;
 const server = app.listen(3000, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+
+  // 0 for success and 1 for unhandled rejection not neccassry to close but a good practice
+  server.close(() => {
+    process.exit(1);
+  });
 });
