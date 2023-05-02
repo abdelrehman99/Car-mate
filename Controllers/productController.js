8c6202f535950e494d3902901e1e9af4ae9d8e39
Abdelrehman <abdelrehmanmamdouh@gmail.com>
2023-05-03 01:40:20 +0300

    fixed bug6

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -247,8 +247,8 @@ const reference = catchAsync(async (session) => {
   // Update Product
   let product = await products.findById(session.client_reference_id);
   product.Buyers.push(user._id);
-  product.Sold = product.Sold + session.line_items.quantity;
-  product.Quantity = product.Quantity - session.line_items.quantity;
+  product.Sold = product.Sold + session.line_items[0].quantity;
+  product.Quantity = product.Quantity - session.line_items[0].quantity;
 
   await products.findByIdAndUpdate(product._id, product, {
     new: true,
