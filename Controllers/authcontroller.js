21d70b28a953c30b18c418535c6516233ab0943c
Abdelrehman <abdelrehmanmamdouh@gmail.com>
2023-06-19 23:45:37 +0300

    Fix arsany bug

--- Controllers/authcontroller.js
+++ Controllers/authcontroller.js
@@ -68,7 +68,7 @@ exports.ProtectRoutes = CatchAsync(async (req, res, next) => {
   // check if the request has a token
   const header = req.headers.authorization;
 
-  console.log(header);
+  console.log(req.headers);
   if (header && header.startsWith('Bearer')) {
     token = header.split(' ')[1];
   }
